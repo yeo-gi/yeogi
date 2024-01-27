c95d2c5243c3e338b9345686e5ee2b9f352b1038
Yeoni <siunaus224@gmail.com>
2024-01-27 14:36:24 +0900

    feat: 프론트 기본 설정 추가 배경색, 폰트, axios 요청 기본 식 등

---
+++ frontend/src/components/community/PostType.tsx
@@ -0,0 +1,5 @@
+export type Post = {
+  postId: number;
+  title: string;
+  content: string;
+};
---
+++ frontend/src/style/NavTheme.tsx
@@ -0,0 +1,9 @@
+import {DefaultTheme} from '@react-navigation/native';
+
+export const NavTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'white',
+  },
+};
