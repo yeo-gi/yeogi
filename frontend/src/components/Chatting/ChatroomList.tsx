f158609521cf9fe2806f24c729ce6f6d3a4cf20b
Yeoni <siunaus224@gmail.com>
2024-01-31 04:06:47 +0900

    feat: 채팅 기능 구현 front 연결 확인, 필요 정보 추가

---
+++ frontend/src/components/Chatting/ChatroomList.tsx
@@ -0,0 +1,41 @@
+import {View, Text, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {getChatroomList} from '../../apis/communityAPI/ChattingAPI';
+import {Chatroom} from './ChatroomType';
+import {useNavi} from '../navigation/useNavi';
+
+export default function ChatroomList() {
+  const [chatrooms, setChatrooms] = useState<Chatroom[]>([]);
+  const navigation = useNavi();
+
+  useEffect(() => {
+    const fetchChatList = async () => {
+      try {
+        const chatList = await getChatroomList();
+        setChatrooms(chatList);
+      } catch (error) {
+        console.error('에러!', error);
+      }
+    };
+
+    fetchChatList();
+  }, []);
+
+  return (
+    <View>
+      {chatrooms.map((chatroom, index) => (
+        <TouchableOpacity
+          key={index}
+          onPress={() =>
+            navigation.navigate('Chatting', {roomId: chatroom.roomId})
+          }>
+          <View key={index}>
+            <Text>{chatroom.chatRoomName}</Text>
+            <Text>{chatroom.createdDate}</Text>
+            <Text>{chatroom.lastChat}</Text>
+          </View>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+}
