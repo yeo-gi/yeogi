e9487778d9d40f05e04a878aebee27173ff5dc45
hallo27 <hallo27@naver.com>
2024-02-01 17:17:29 +0900

    feat: 체크박스 isBlue 속성 추가

--- frontend/src/components/common/CustomCheckBox.tsx
+++ frontend/src/components/common/CustomCheckBox.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Pressable, StyleSheet, View, ViewStyle, Text} from 'react-native';
 import {customColor} from '../../style/common/CommonStyle';
 import {userStyles} from '../../style/user/UserStyles';
@@ -11,6 +11,7 @@ type CheckContent = {
   children?: React.ReactNode;
   style?: ViewStyle;
   title?: string;
+  isBlue?: boolean;
 };
 
 export default function CustomCheckBox(props: CheckContent) {
@@ -20,37 +21,53 @@ export default function CustomCheckBox(props: CheckContent) {
     }
   };
 
+  const styles = Styles(props.isBlue);
+  const [checkColor, setCheckcolor] = useState('white');
+
+  useEffect(() => {
+    if (!props.isChecked || props.isBlue === true) {
+      setCheckcolor('white');
+    } else {
+      setCheckcolor('black');
+    }
+  }, [props.isChecked, props.isBlue]);
+
   return (
     <View style={styles.container}>
       <Pressable
         disabled={props.disabled}
         onPress={onPressedHandler}
         style={[styles.checkbox, props.isChecked && styles.checked]}>
-        <Entypo name="check" color={props.isChecked ? 'black' : 'white'} />
+        <Entypo name="check" color={checkColor} />
       </Pressable>
       <Text style={[userStyles.title, {marginLeft: 9}]}>{props.title}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-  },
-  checkbox: {
-    height: 18,
-    width: 18,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    borderColor: customColor.gray30,
-    borderWidth: 1,
-  },
-  checked: {
-    backgroundColor: customColor.gray30,
-    borderColor: customColor.gray30,
-  },
-  label: {
-    marginLeft: 8,
-  },
-});
+function Styles(isBlue?: boolean) {
+  const color = isBlue ? customColor.blue : null;
+
+  return StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+    },
+    checkbox: {
+      height: 18,
+      width: 18,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'white',
+      borderColor: color ?? customColor.gray30,
+      borderWidth: 1,
+      borderRadius: 2,
+    },
+    checked: {
+      backgroundColor: color ?? customColor.gray30,
+      borderColor: color ?? customColor.gray30,
+    },
+    label: {
+      marginLeft: 8,
+    },
+  });
+}
