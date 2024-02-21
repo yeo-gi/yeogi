cc4a78c175a104f11db1af54e79edde72c7b6ec3
hallo27 <hallo27@naver.com>
2024-02-22 01:24:18 +0900

    feat: 홈 화면 MD 추천 여행지, 지난 여행 컴포넌트 추가

--- frontend/src/style/home/HomeStyle.tsx
+++ frontend/src/style/home/HomeStyle.tsx
@@ -1,10 +1,18 @@
 import {StyleSheet} from 'react-native';
 import {windowWidth} from '../../hooks/Dimensions';
+import {customFont} from '../common/CommonStyle';
 
 export const styles = StyleSheet.create({
   image: {
     width: windowWidth,
     height: 156,
     marginVertical: 22,
+  },
+  title: {
+    fontSize: 22,
+    letterSpacing: 0.35,
+    lineHeight: 28,
+    fontFamily: customFont.bold,
+    color: 'black',
   },
 });
